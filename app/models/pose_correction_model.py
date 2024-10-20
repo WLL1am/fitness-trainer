b7e1f01fd72758484568207a3b5d5e1e4a942a74
William Chung <118134309+WLL1am@users.noreply.github.com>
2024-10-19 20:38:12 -0700

    init rasa_bot with simple intents, fixed bugs in pose_correction_model

--- app/models/pose_correction_model.py
+++ app/models/pose_correction_model.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 class PoseCorrectionModel(nn.Module):
     def __init__(self):
         super(PoseCorrectionModel, self).__init__()
-        self.fc1 = nn.Lienar(34, 64)
+        self.fc1 = nn.Linear(34, 64)
         self.fc1 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 3)
         
     def forward(self, x):
         x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc(x))
+        x = torch.relu(self.fc2(x))
         
         return self.fc3(x)
     
